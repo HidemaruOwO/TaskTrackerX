6dd43e8e2ec3865cd48f05a72813cb495314b20c
HidemaruOwO <hidemal@outlook.jp>
2023-04-22 22:24:57 +0900

    :snowman: prettierでlintとformat (.eslintrc.json, package.json, src/commands/changePrefix.ts, src/commands/greet.ts, src/events/guildCreate.ts, src/events/interactionCreate.ts, src/events/messageCreate.ts, src/events/ready.ts, src/functions.ts, src/handlers/Command.ts, src/handlers/Event.ts, src/index.ts, src/schemas/Guild.ts, src/slashCommands/clear.ts, src/slashCommands/embed.ts, src/slashCommands/ping.ts, src/types.d.ts, yarn.lock)

--- src/commands/changePrefix.ts
+++ src/commands/changePrefix.ts
@@ -1,17 +1,17 @@
-import { setGuildOption } from "../functions";
-import { Command } from "../types";
+import { setGuildOption } from '../functions';
+import { Command } from '../types';
 
 const command: Command = {
-    name: "changePrefix",
-    execute: (message, args) => {
-        let prefix = args[1]
-        if (!prefix) return message.channel.send("No prefix provided")
-        if (!message.guild) return;
-        setGuildOption(message.guild, "prefix", prefix)
-        message.channel.send("Prefix successfully changed!")
-    },
-    permissions: ["Administrator"],
-    aliases: []
-}
+  name: 'changePrefix',
+  execute: (message, args) => {
+    const prefix = args[1];
+    if (!prefix) return message.channel.send('No prefix provided');
+    if (!message.guild) return;
+    setGuildOption(message.guild, 'prefix', prefix);
+    message.channel.send('Prefix successfully changed!');
+  },
+  permissions: ['Administrator'],
+  aliases: [],
+};
 
-export default command
+export default command;
