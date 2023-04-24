42ceeeaa79ba7813073d9ef15d51bd99d0529908
HidemaruOwO <hidemal@outlook.jp>
2023-04-24 20:17:30 +0900

    ✨ 複数のサブコマンドの追加 (src/slashCommands/todo.ts)

--- src/slashCommands/todo.ts
+++ src/slashCommands/todo.ts
@@ -5,7 +5,50 @@ import { SlashCommand } from '../types';
 const command: SlashCommand = {
   command: new SlashCommandBuilder()
     .setName('todo')
-    .setDescription('Show help for the ToDo command'),
+    .setDescription('Show help for the ToDo command')
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName('add')
+        .setDescription('Create ToDo')
+        .addStringOption((option) =>
+          option
+            .setName('title')
+            .setDescription('Title of ToDo')
+            .setRequired(true),
+        )
+        .addStringOption((option) =>
+          option
+            .setName('value')
+            .setDescription('Contents of ToDo')
+            .setRequired(true),
+        ),
+    )
+    .addSubcommand((subcommand) =>
+      subcommand.setName('delete').setDescription('Delete ToDo'),
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName('list')
+        .setDescription('Show ToDo')
+        .addBooleanOption((option) =>
+          option
+            .setName('isshowdone')
+            .setDescription('Do you want to view completed tasks ?')
+            .setRequired(true),
+        ),
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName('setchannel')
+        .setDescription('Set a channel to notify you that you have set a ToDo')
+        .addChannelOption((option) =>
+          option
+            .setName('channel')
+            .setDescription('Please select a channel for notification')
+            .setRequired(true),
+        ),
+    ),
+
   execute: (interaction) => {
     const commands: string[] = [
       '`/todo add <title> <value>`: Create ToDo',
